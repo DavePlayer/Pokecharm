3a9cb912f9f9086045623150a4ac48751edf01a9
DavePlayer <daveplayer2001@gmail.com>
2020-06-17 17:21:16 +0200

    added react router donm to project and set up basic routes

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 
 export class App extends React.Component {
     constructor(props:any){
@@ -7,7 +9,28 @@ export class App extends React.Component {
     
     render(){
         return(
-            <h1>ready perfectly</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' render={() => <WelcomeWrapper children={<h1>home</h1>} />} />
+                    <Route exact path='/login' render={() => <h1>login</h1>} />
+                    <Route exact path='/register' render={() => <h1>register</h1>} />
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
+
+interface IPropHolder {
+    children?: React.ReactNode
+}
+
+const WelcomeWrapper:React.FC = (props:IPropHolder) => (
+    <>
+        <h1>header</h1>
+        {
+            (props.children && props.children)
+        
+        }
+        <h1>footer</h1>
+    </>
+)
