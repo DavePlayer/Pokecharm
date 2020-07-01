93b88105febfa5135bfb269f744c656c391dc9ff
DavePlayer <daveplayer2001@gmail.com>
2020-07-01 19:16:57 +0200

    adjusted styles and pokemonBlock to read data from objects

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,13 +1,42 @@
 import * as React from 'react'
 import { PokemonBlock } from './pokemonBlock'
 
+
 export const HomePage = () => {
+    const palceBacks = [
+        {
+            name: 'Bulbasaur',
+            id: 1,
+            status: 'caught',
+            imgUrl: `https://pokeres.bastionbot.org/images/pokemon/1.png`
+        },
+        {
+            name: 'Bulbasaur',
+            id: 62,
+            status: 'uncaught',
+            imgUrl: `https://pokeres.bastionbot.org/images/pokemon/62.png`
+        },
+        {
+            name: 'Bulbasaur',
+            id: 633,
+            status: 'caught',
+            imgUrl: `https://pokeres.bastionbot.org/images/pokemon/633.png`
+        },
+        {
+            name: 'Bulbasaur',
+            id: 733,
+            status: 'uncaught',
+            imgUrl: `https://pokeres.bastionbot.org/images/pokemon/733.png`
+        },
+    ]
+    let delay=0
     return (
         <main className="HomePage">
-            <PokemonBlock name='Bulbasaur' id={1} status="caught" />
-            <PokemonBlock name='Bulbasaur' id={62} status="caught" />
-            <PokemonBlock name='Bulbasaur' id={633} status="caught" />
-            <PokemonBlock name='Bulbasaur' id={1024} status="caught" />
+            {
+                palceBacks.map( (o:any) => {
+                    return <PokemonBlock delay={delay+=0.2} imgUrl={o.imgUrl} name={o.name} id={o.id} status={o.status} />
+                })
+            }
         </main>
     )
 }
--- frontend/src/components/pokemonBlock.tsx
+++ frontend/src/components/pokemonBlock.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react'
+import { motion } from 'framer-motion'
 
 interface IProps {
     id: number,
     name: string,
-    status: string
+    status: string,
+    imgUrl: string,
+    delay: number
 }
 
 export const PokemonBlock:React.FC<IProps> = (props) => {
+    const pokeVariant = {
+        initial: {
+            scale: 0
+        },
+        animate: {
+            scale: 1,
+            transition: {
+                duration: 0.3,
+                delay: props.delay
+            }
+        }
+    }
     return (
-        <section className='PokeBlock'>
+        <motion.section variants={pokeVariant} initial='initial' animate='animate' className='PokeBlock'>
             <header>
                 {
                     props.id <= 9 ?
@@ -21,8 +36,15 @@ export const PokemonBlock:React.FC<IProps> = (props) => {
                 }
                 <div className="name">{props.name}</div>
             </header>
-            <figure><img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="Bulbasaur"/></figure>
-            <article className='status'>Status: {props.status}</article>
-        </section>
+            <figure><img src={props.imgUrl} alt="Bulbasaur"/></figure>
+            <article>
+                Status: 
+                {
+                    props.status=='caught'? <span className='blue'> {props.status}</span>
+                    :
+                    <span className='red'> {props.status}</span>
+                }
+            </article>
+        </motion.section>
     )
 }
