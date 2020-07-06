4356c8f18259f5cb2aa025ccc8a6301ed2df5a8a
DavePlayer <daveplayer2001@gmail.com>
2020-07-06 16:29:57 +0200

    added radar chart and changed organisation of elements on pokemonDetails page

--- frontend/src/components/pokemonDetails.tsx
+++ frontend/src/components/pokemonDetails.tsx
@@ -3,6 +3,7 @@ import Axios from 'axios'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Radar } from 'react-chartjs-2'
 
 const mainVariant = {
     initial:{x: '-100vw'},
@@ -18,20 +19,45 @@ export const PokemonDetails:React.FC = () => {
     let { id } = useParams()
     const [data, setData]:any = useState()
     const [dex, setDex]:any = useState()
+    const [chart, setChart]:any = useState()
     const [fetched, setfetched] = useState(false)
     useEffect(() => {
         Axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then( o => {
             setData(o.data)
+                console.log('stat ', o.data.stats[0].base_stat)
+                setChart({
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    type: 'radar',
+                    labels: ['Hp', 'Attack', 'Defense', 'Special Attack', 'Special Defense', 
+                        'Speed'],
+                    datasets: [
+                        {
+                            label: 'Stats',
+                            data: [
+                                o.data.stats[0].base_stat,
+                                o.data.stats[1].base_stat,
+                                o.data.stats[2].base_stat,
+                                o.data.stats[3].base_stat,
+                                o.data.stats[4].base_stat,
+                                o.data.stats[5].base_stat,
+                            ],
+                            borderColor: 'rgba(255, 255, 255, 1)',
+                            pointBackgroundColor: 'rgba(255, 255, 255, 1)',
+                            pointBorderWidth: 6
+                        }
+                    ]
+            
+                })
             setfetched(true)
         })
         Axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
         .then((o:any) => {
             const something = o.data.flavor_text_entries.filter( (oo:any) => {
-                console.log(oo.version.name == 'x')
-                return (oo.version.name == 'x' && oo.language.name == 'en') ? oo.version.name : null
+                return (oo.language.name == 'en') ? oo.version.name : null
             })
-            setDex( ...something )
+            setDex( something[0] )
             console.log(`dexxxxx: ${dex}`)
             console.log(something)
         })
@@ -63,13 +89,38 @@ export const PokemonDetails:React.FC = () => {
                                 </div>
                         </article>
                     </section>
+                    <section className="chart">
+                        <Radar
+                            data={chart}
+                            options={{
+                                responsive:true,
+                                maintainAspectRatio: false,
+                                legend: {
+                                    display: false, 
+                                },
+                                scale: {
+                                    angleLines: {
+                                        display: false
+                                    },
+                                    pointLabels: {
+                                        fontColor: 'rgba(255, 255, 255, 1)'
+                                    }, 
+                                    ticks: {
+                                        suggestedMin: 20,
+                                        suggestedMax: 150,
+                                        display: false
+                                    },
+                                    gridLines: {
+                                        color: 'rgba(255, 255, 255, 0.6)'
+                                    }
+                                }
+                            }}
+                        />
+                    </section>
                     <section className="pokemonStatus">
                         <h1>Change Status</h1>
                         <button className="caught">Normal Caught</button>
                         <button className="uncaught">Shiny Uncaught</button>
-                    </section>
-                    <section className="chart">
-                        <p>chart</p>
                     </section>
                     <section className="pokedexEntry">
                         <p>{dex.flavor_text}</p>
