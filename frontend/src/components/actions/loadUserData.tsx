911e831f30b526bac1f7f0b9561b8487955a1037
DavePlayer <daveplayer2001@gmail.com>
2020-07-16 15:27:03 +0200

    added error handler when veryfying token

--- frontend/src/components/actions/loadUserData.tsx
+++ frontend/src/components/actions/loadUserData.tsx
@@ -7,11 +7,13 @@ export const loadUserData = () => {
             Axios.get('http://127.0.0.1:7200/auth/test', {headers: {'authorization': token}} )
             .then( (o:any) => {
                 console.log('token verify fetched')
-                console.log(o.data)
                 if(o.data.status != 'error'){
                     console.log('token is valid')
                     dispatch({type: 'LOAD_USER_DATA' ,payload :{isUserLoged: true, token}})
                 } else return dispatch({type: 'LOAD_USER_DATA' ,payload: {isUserLoged: false}})
+            })
+            .catch( o => {
+                localStorage.removeItem('token')
             })
         } else {
             console.log('no token saved')
