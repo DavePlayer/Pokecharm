79327a6bacc12b99b3839c41f13fb9a74095da36
DavePlayer <daveplayer2001@gmail.com>
2020-07-18 20:38:14 +0200

    added functionality to fetch game versions and based on them pokedexes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { Logo } from './logo'
 import { motion, AnimatePresence } from 'framer-motion'
+import { useEffect, useState } from 'react'
+import Axios, { AxiosResponse } from 'axios'
+import { filterPokedexGameGroup } from '../filterPokedexes'
 
 interface Iprops {
     path?: any
@@ -26,7 +29,56 @@ const h1Variant:any = {
 }
 
 export const Header:React.FC<Iprops> = (props) => {
-    console.log(props.path)
+    const [gameVersions, setGameVersions] = useState<Array<{name: string, url: string}>>([])
+    const [selectedVersion, setSelectedVersion] = useState<number>(1)
+    const [dex, setDex] = useState<number>(1)
+    const [isLoading, setIsLoading] = useState<boolean>(false)    
+    const [pokedexes, setPokedexes] = useState<Array<{name: string, url: string}>>([])    
+
+    useEffect(() => {
+        setIsLoading(true)
+        Axios.get('https://pokeapi.co/api/v2/version/?limit=1000')
+            .then( (o:AxiosResponse) => {
+                console.log(o.data)
+                setGameVersions(o.data.results)
+            }).finally( () => {
+                const dexIndex:number = filterPokedexGameGroup(selectedVersion)
+                console.log(dexIndex)
+                Axios.get(`https://pokeapi.co/api/v2/version-group/${dexIndex}`)
+                .then( o => {
+                    console.log(o.data.pokedexes)
+                    setPokedexes(o.data.pokedexes)
+                })
+                .finally( () => {
+                    setIsLoading(false)
+                })
+            })
+    }, [])
+
+    const handleSelectGame = (e:any, index:number) => {
+        const lis = document.querySelectorAll('.gameVersions > li')
+        Array.prototype.map.call(lis, (o:any) => {
+            o.setAttribute('class', '')
+        })
+        e.target.classList += 'selected'
+        setSelectedVersion(index)
+
+        const dexIndex:number = filterPokedexGameGroup((index + 1))
+        console.log('dex index', dexIndex, ' ', index + 1)
+        Axios.get(`https://pokeapi.co/api/v2/version-group/${dexIndex}`)
+        .then( o => {
+            setPokedexes(o.data.pokedexes)
+        })
+    }
+    const handleSelectDex = (e:any, index:number) => {
+        const lis = document.querySelectorAll('.Dexes > li')
+        Array.prototype.map.call(lis, (o:any) => {
+            o.setAttribute('class', '')
+        })
+        e.target.classList += 'selected'
+        setDex(index)
+    }
+
     return (
         <motion.header initial={{y: '-50vh'}} animate={{y: 0}} transition={{duration: 0.6}} className="mainHeader">
             <section className="logoHolder">
@@ -43,15 +95,51 @@ export const Header:React.FC<Iprops> = (props) => {
             <section className="filters">
                 <ul>
                     <li>Game Version
-                        <ol>
-                            <li>xd</li>
-                            <li>xd</li>
-                            <li>xd</li>
+                        <ol className='gameVersions'>
+                            {
+                                !isLoading && gameVersions.length > 0 ? 
+                                    gameVersions.map( (o, i:number) => {
+                                        return(
+                                            i == 0 ?
+                                            <li className='selected' onClick={(e) => handleSelectGame(e, i)} key={o.name}>{o.name}</li>
+                                            :
+                                            <li onClick={(e) => handleSelectGame(e, i)} key={o.name}>{o.name}</li>
+                                        )
+                                    })
+                                :
+                                    <li>nie działa</li>
+                            }
                         </ol>
                     </li>
-                    <li>Status Filter</li>
-                    <li>Shiny Helper</li>
-                    <li>Pokedex</li>
+                    <li>Status Filter
+                        <ol>
+                            <li>On</li>
+                            <li>Off</li>
+                        </ol>
+                    </li>
+                    <li>Shiny Helper
+                        <ol>
+                            <li>On</li>
+                            <li>Off</li>
+                        </ol>
+                    </li>
+                    <li>Pokedex
+                        <ol className='Dexes'>
+                            {
+                                !isLoading && pokedexes.length > 0 ? 
+                                pokedexes.map( (o, i:number) => {
+                                    return(
+                                        i == 0 ?
+                                        <li className='selected' onClick={(e) => handleSelectDex(e, i)} key={o.name}>{o.name}</li>
+                                        :
+                                        <li onClick={(e) => handleSelectDex(e, i)} key={o.name}>{o.name}</li>
+                                    )
+                                })
+                            :
+                                <li>nie działa</li>
+                            }
+                        </ol>
+                    </li>
                 </ul>
             </section>
         </motion.header>
---
+++ frontend/src/filterPokedexes.ts
@@ -0,0 +1,52 @@
+export const filterPokedexGameGroup = (index:number):number => {
+    switch (index) {
+        case 1:
+        case 2:
+            return 1
+        case 3:
+            return 2
+        case 4: 
+        case 5:
+            return 3
+        case 6:
+            return 4
+        case 7: 
+        case 8:
+            return 5
+        case 9:
+            return 6
+        case 10:
+        case 11:
+            return 7
+        case 12:
+        case 13:
+            return 8
+        case 14:
+            return 9
+        case 15:
+        case 16:
+            return 10
+        case 17:
+        case 18:
+            return 11
+        case 19:
+            return 12
+        case 20:
+            return 13
+        case 21:
+        case 22:
+            return 14
+        case 23:
+        case 24:
+            return 15
+        case 25:
+        case 26:
+            return 16
+        case 27:
+        case 28:
+            return 17
+        case 29: 
+        case 30:
+            return 18
+    }
+}
