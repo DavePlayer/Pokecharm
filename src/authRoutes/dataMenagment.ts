142b234a977cc5f2810ce9b8abd3d13aecc3445f
DavePlayer <daveplayer2001@gmail.com>
2020-07-20 18:15:23 +0200

    added filtering game version functionality

--- src/authRoutes/dataMenagment.ts
+++ src/authRoutes/dataMenagment.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/jwtVerify'
 import DataBase from '../database'
+import { getTableName } from '../getTableName'
 
 export const dataRouter: express.Router = express.Router()
 
 dataRouter.get('/getCatchStatus', verifyToken ,(req: any, res: express.Response) => {
-    DataBase.fetchCatchStatus(req.id, req.query.tableName)
+    const tableName = getTableName(req.query.gameVersion, req.query.pokedex)
+    DataBase.fetchCatchStatus(req.id, tableName)
     .then( o => res.json(o))
     .catch(o => res.json(o))
 })
---
+++ src/getTableName.ts
@@ -0,0 +1,90 @@
+export const getTableName = (gameVersion:string, pokedex:string):string => {
+    let table = ``
+    switch(gameVersion){
+        case 'black-2':
+            table += 'black2'
+            break
+        case 'white-2':
+            table += 'white2'
+            break
+        case 'ultra-sun':
+            table += 'ultrasun'
+            break
+        case 'ultra-moon':
+            table += 'ultramoon'
+            break
+        case 'alpha-sapphire':
+            table += 'alphasapphire'
+            break
+        case 'omega-ruby':
+            table += 'omegaruby'
+            break
+        default:
+            table += gameVersion
+            break
+    }
+    switch(pokedex){
+        case 'original-johto':
+            table += 'johto'
+            break
+        case 'original-sinnoh':
+            table += 'sinnoh'
+            break
+        case 'extended-sinnoh':
+            table += 'sinnoh'
+            break
+        case 'updated-johto':
+            table += 'johto'
+            break
+        case 'original-unova':
+            table += 'unova'
+            break
+        case 'updated-unova':
+            table += 'unova'
+            break
+        case 'kalos-central':
+            table += 'kaloscentral'
+            break
+        case 'kalos-coastal':
+            table += 'kaloscoastal'
+            break
+        case 'kalos-mountain':
+            table += 'kalosmountain'
+            break
+        case 'updated-hoenn':
+            table += 'updatedhoenn'
+            break
+        case 'original-alola':
+            table += 'alola'
+            break
+        case 'original-melemele':
+            table += 'melemele'
+            break
+        case 'original-akala':
+            table += 'akala'
+            break
+        case 'original-ulaula':
+            table += 'ulaula'
+            break
+        case 'original-poni':
+            table += 'poni'
+            break
+        case 'updated-alola':
+            table += 'alola'
+            break
+        case 'updated-melemele':
+            table += 'melemele'
+            break
+        case 'updated-akala':
+            table += 'akala'
+            break
+        case 'updated-ulaula':
+            table += 'ulaula'
+            break
+        case 'updated-poni':
+            table += 'poni'
+            break
+               
+    }
+    return table
+}
